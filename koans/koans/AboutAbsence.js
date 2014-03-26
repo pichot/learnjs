89d1dd9cd73af75f56823d98fa77fc49a4a90338
Jonathan Pichot <jonathan@pichot.us>
2014-03-26 19:56:56 -0400

    Messing around

--- koans/koans/AboutAbsence.js
+++ koans/koans/AboutAbsence.js
@@ -1,6 +1,6 @@
 describe('about absence', function() {
 
-  it('should have a type', function() {
+  it('should have a change', function() {
     expect(null).toHaveType(FILL_ME_IN);
     expect(undefined).toHaveType(FILL_ME_IN);
     expect(aVariableCalledFromOutOfTheAbyss).toHaveType(FILL_ME_IN);
